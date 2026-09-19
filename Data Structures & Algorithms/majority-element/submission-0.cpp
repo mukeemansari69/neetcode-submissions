ea0257b544d2f56f34ec69b7da700cbedb4f55ca
Mukeem Akhatar <116939397+mukeemansari69@users.noreply.github.com>
2026-09-19 10:25:22 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        int t=nums.size()/2;
+        for(int i=0;i<nums.size();i++){
+           
+                if(mp[nums[i]]>t) return nums[i];
+            
+        }
+        return-1;
+    }
+};
